db791527dac0a1e4e1c39455ec33574199525aac
HeitorCarvalho26 <heitorcmiguel@gmail.com>
2026-02-09 10:42:35 -0300

    refact: declarando explicitamente os tipos

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 from models.tarefa import Tarefa
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template("home.html", titulo='Home')
+    return render_template('home.html', titulo='Home')
 
 @app.route('/agenda', methods=['GET', 'POST'])
 def agenda():
-    tarefa=None
+    tarefas = None
     
     if request.method == 'POST':
         titulo_tarefa = request.form['titulo-tarefa']
@@ -20,11 +20,14 @@ def agenda():
     tarefas = Tarefa.obter_tarefas()
     return render_template('agenda.html', titulo='Agenda', tarefas=tarefas)
 
-@app.route('/tarefa/<int:idTarefa>')
+@app.route('/delete/<int:idTarefa>')
 def delete(idTarefa):
     tarefa = Tarefa.id(idTarefa)
-    tarefa.exculir_tarefas()
+    tarefa.excluir_tarefa()
+    # return render_template('agenda.html', titulo="Agenda", tarefas=tarefas)
+    return redirect(url_for('agenda'))
 
 @app.route('/ola')
+
 def ola_mundo():
-    return 'Olá, Mundo!'
+    return "Olá, Mundo!"
--- models/database.py
+++ models/database.py
@@ -1,44 +1,61 @@
-from sqlite3 import Connection, Cursor, connect
-from typing import Any
+from types import TracebackType
+from sqlite3 import Connection, connect, Cursor
+from typing import Any, Optional, Self, Type
+import traceback
+
+
+from click import Option
 
 class Database:
     def __init__(self, db_name: str) -> None:
         self.connection: Connection = connect(db_name)
         self.cursor: Cursor = self.connection.cursor()
 
-    def execute(self, query:str, params: tuple = ()) -> Cursor:
+    def executar(self, query: str, params: tuple = ()) -> Cursor:
         self.cursor.execute(query, params)
         self.connection.commit()
         return self.cursor
     
-    def buscar_tudo(self, query:str, params: tuple = ()) -> list[Any]:
+    def buscar_tudo(self, query: str, params: tuple = ()) -> list[Any]:
         self.cursor.execute(query, params)
         return self.cursor.fetchall()
     
     def close(self) -> None:
         self.connection.close()
 
-# Metodos para o gerenciamento de contexto
-
-    # Metodo de entrado no contexto
-    def __enter__(self):
+    
+    # Métodos para o gerenciamento de contexto
+    # Método de entrada no contexto
+    def __enter__(self) -> Self:
         return self
+    
+    # Método de saída do contexto
+    def __exit__(
+            self,
+            exc_type: Optional[Type[BaseException]],
+            exc_value: Optional[BaseException] 
+            tb: Optional[TracebackType]) -> None:
+        
+        if exc_type is not None:
+            print('Exceção capturada no contexto:')
+            print(f'Tipo: {exc_type.__name__}')
+            print(f'Mensagem: {exc_value}')
+            print('Traceback completo:')
+            traceback.print_tb(tb)
 
-    # Metodo de saida no contexto    
-    def __exit__(self, exc_type, exc_value, traceback):
         self.close()
 
-# Área de testes
+
+# Área de Testes
 # try:
 #     db = Database('./data/tarefas.sqlite3')
-#     db.execute('''
+#     db.executar('''
 #     CREATE TABLE IF NOT EXISTS tarefas (
-#         id  INTEGER PRIMARY KEY AUTOINCREMENT,
+#         id INTEGER PRIMARY KEY AUTOINCREMENT,
 #         titulo_tarefa TEXT NOT NULL,
-#         data_conclusao TEXT)
+#         data_conclusao TEXT);
 #     ''')
-
-#     db.execute('INSERT INTO tarefas (titulo_tarefa, data_conclusao) VALUES (?, ?);', ('Estudar Python', '2026-02-02'))
+#     db.executar('INSERT INTO tarefas (titulo_tarefa, data_conclusao) VALUES (?, ?);', ('Estudar Python', '2026-02-02'))
 # except Exception as e:
 #     print(f"Erro ao criar a tabela: {e}")
 # finally:
